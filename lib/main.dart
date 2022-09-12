3f94be337a8b53b4f97d8b192a66e091c07a1629
Pedro Schiavom <pedro.schiavon@mcs-it.com.br>
2022-09-12 18:01:53 -0300

    new components of transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,19 @@
-
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_list.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
- 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage());
+      home: MyHomePage(),
+    );
   }
 }
 
-
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis',
-      value: 310.76, 
-      date: DateTime.now(),
-      ),
-      Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30, 
-      date: DateTime.now(),
-      ),
-  ];
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +21,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -49,13 +32,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-           children: _transactions.map((tr){
-              return Card(
-                child: Text(tr.title),
-              );
-            }).toList()
-          )
+          TransactionUser(),
         ],
       ),
     );
