2762d2f0197e398c7ba5a39be7eb7cf1701318ad
Pedro Schiavom <pedro.schiavon@mcs-it.com.br>
2022-10-20 11:39:05 -0300

    transaction user

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,33 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'transaction_form.dart';
 import 'transaction_list.dart';
 import '../models/transaction.dart';
-class TransactionUser extends StatefulWidget {
- 
 
+class TransactionUser extends StatefulWidget {
   @override
   State<TransactionUser> createState() => _TransactionUserState();
 }
 
 class _TransactionUserState extends State<TransactionUser> {
-
   final _transactions = [
     Transaction(
       id: 't1',
       title: 'Novo Tênis',
-      value: 310.76, 
+      value: 310.76,
       date: DateTime.now(),
-      ),
-      Transaction(
+    ),
+    Transaction(
       id: 't2',
       title: 'Conta de Luz',
-      value: 211.30, 
+      value: 211.30,
       date: DateTime.now(),
-      ),
+    ),
   ];
 
-  final teste = false;
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +46,6 @@ class _TransactionUserState extends State<TransactionUser> {
       children: <Widget>[
         TransactionList(_transactions),
         TransactionForm(),
-        if(teste == true)Text('olá'),
       ],
     );
   }
